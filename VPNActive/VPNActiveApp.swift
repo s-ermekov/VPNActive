2b39ed76fc37190f20b861c7fab2f4139aae41de
Sanzhar Ermekov <ermekovss@yandex.ru>
2023-04-30 20:14:45 +0300

    Initial Commit

---
+++ VPNActive/VPNActiveApp.swift
@@ -0,0 +1,17 @@
+//
+//  VPNActiveApp.swift
+//  VPNActive
+//
+//  Created by Санжар Эрмеков on 30.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct VPNActiveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
